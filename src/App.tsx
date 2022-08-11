2cb019b5e946df307c33c22ed25d67675c38702b
ana.laclau <ana.laclau@mediamonks.com>
2022-08-11 18:34:50 -0300

    Implemented NavigationContainer and temporary commented rn gesture handler import

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
+// import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+// import {StyleSheet, View} from 'react-native';
 import Albums from './views/Albums';
 import Photos from './views/Photos';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       {/* <Albums /> */}
       <Photos />
-    </View>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#bbb891',
-  },
-});
 
 export default App;
