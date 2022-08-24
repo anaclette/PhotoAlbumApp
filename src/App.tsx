9cf6b5ac0cde58a699a66f8ec62ff6625d613089
ana.laclau <ana.laclau@mediamonks.com>
2022-08-24 12:07:46 -0300

    Updated App file with context state and stack navigator screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {StyleSheet, View} from 'react-native';
-import Albums from './views/Albums';
-import Photos from './views/Photos';
+import {AuthProvider} from './state/Auth';
+import {StackNavigator} from './navigator/StackNavigator';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      {/* <Albums /> */}
-      <Photos />
-    </NavigationContainer>
+    <AppState>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </AppState>
   );
+};
+
+const AppState = ({children}: {children: JSX.Element}) => {
+  return <AuthProvider>{children}</AuthProvider>;
 };
 
 export default App;
