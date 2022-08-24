1a84db8608669086bbafcf0770c8eab50a59f461
ana.laclau <ana.laclau@mediamonks.com>
2022-08-24 13:31:49 -0300

    Removed commented and unused code

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,24 +1,13 @@
 export const API_HOST = 'https://jsonplaceholder.typicode.com';
-export const PHOTO_DETAIL_URL = 'https://via.placeholder.com';
 
 export enum ENDPOINTS {
   ALBUMS = 'albums',
   PHOTOS = 'photos',
 }
 
-// const queryString = (params: string) => {
-//   Object.keys(params)
-//     .map((key: string) => `${key}=${params[key]}`)
-//     .join('&');
-// };
-
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
-
-// export const createUrl = (url: string, queryOptions: string) => {
-//   return `${url}?${queryString(queryOptions)}`;
-// };
 
 export const randomIcon = () => {
   const iconNames = [
