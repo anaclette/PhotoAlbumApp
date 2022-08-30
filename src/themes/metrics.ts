cc425b50903bd49fc909d3228184b59c0759b05f
ana.laclau <ana.laclau@mediamonks.com>
2022-08-30 14:48:14 -0300

    Removed unused variables

--- src/themes/metrics.ts
+++ src/themes/metrics.ts
@@ -1,17 +1,10 @@
 import {Dimensions} from 'react-native';
-import {
-  ms as RNModScale,
-  mvs as RNModVScale,
-  s as RNScale,
-  vs as RNVerticalScale,
-} from 'react-native-size-matters';
+import {s as RNScale, vs as RNVerticalScale} from 'react-native-size-matters';
 const {height, width} = Dimensions.get('window');
 
 const scale = (size: number) => RNScale(size);
 const scaleVertical = (size: number) => RNVerticalScale(size);
-const modScale = (size: number, factor?: number) => RNModScale(size, factor);
-const modScaleVertical = (size: number, factor?: number) =>
-  RNModVScale(size, factor);
+
 const HEIGHT_CUT_OFF = 600;
 const scaledFontSize = (size: number) =>
   height > HEIGHT_CUT_OFF ? size : size * (height / (HEIGHT_CUT_OFF * 1.1));
@@ -22,6 +15,4 @@ export default {
   screenWidth: width,
   scale,
   scaleVertical,
-  modScale,
-  modScaleVertical,
 };
