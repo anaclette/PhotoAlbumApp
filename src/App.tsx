bc1d3108d54b44807056ed5a580dced627f546d6
ana.laclau <ana.laclau@mediamonks.com>
2022-09-06 17:45:09 -0300

    Left commented out persisted state in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import 'react-native-gesture-handler';
-
 import React from 'react';
+import {Provider} from 'react-redux';
+import {getPersistor, getStore} from './state/store';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthProvider} from './state/Auth';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 import {StackNavigator} from './navigation/StackNavigator';
+
+const store = getStore();
+const persistor = getPersistor();
 
 const App = () => {
   return (
+    // <Provider store={store}>
+    //   <PersistGate persistor={persistor}>
+    //     <NavigationContainer>
+    //       <StackNavigator />
+    //     </NavigationContainer>
+    //   </PersistGate>
+    // </Provider>
     <AppState>
       <NavigationContainer>
         <StackNavigator />
