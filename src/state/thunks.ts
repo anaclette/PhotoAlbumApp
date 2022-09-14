6c6f615c9e72f0027929b0e7cae84d1899890f65
ana.laclau <ana.laclau@mediamonks.com>
2022-09-14 15:04:33 -0300

    Removed console logs and handled errors in async thunks

--- src/state/thunks.ts
+++ src/state/thunks.ts
@@ -3,30 +3,28 @@ import {ACTIONS, API_HOST, STATE_MODULES} from '../utils/variables';
 
 export const getPhotos = createAsyncThunk(
   `${STATE_MODULES.PHOTOS}/${ACTIONS.GET_PHOTOS}`,
-  async () => {
+  async (_, {rejectWithValue}) => {
     try {
       const photos = await fetch(`${API_HOST}/${STATE_MODULES.PHOTOS}`)
         .then(response => response.json())
         .then(data => data);
-      console.log('photos from thunk: ', photos);
       return photos;
     } catch (error) {
-      console.error(error);
+      rejectWithValue(error);
     }
   },
 );
 
 export const getAlbums = createAsyncThunk(
   `${STATE_MODULES.ALBUMS}/${ACTIONS.GET_ALBUMS}`,
-  async () => {
+  async (_, {rejectWithValue}) => {
     try {
       const albums = await fetch(`${API_HOST}/${STATE_MODULES.ALBUMS}`)
         .then(response => response.json())
         .then(data => data);
-      console.log('albums from thunk: ', albums);
       return albums;
     } catch (error) {
-      console.warn('Failed to fetch albums');
+      rejectWithValue(error);
     }
   },
 );
